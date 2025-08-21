7bfac67ad6cd973d2ed2a9dc2165968bb9d82b0c
Eugene Hong <awidesky@naver.com>
2025-08-21 12:24:36 +0900

    add InvalidEncodingTest stub

---
+++ src/main/java/io/github/awidesky/myUtils/InvalidEncodingTest.java
@@ -0,0 +1,14 @@
+package io.github.awidesky.myUtils;
+
+import io.github.awidesky.randomTextGenerator.RandomTextGenerator;
+import io.github.awidesky.randomTextGenerator.UnicodeLanguage;
+
+public class InvalidEncodingTest {
+
+	RandomTextGenerator rtg = new RandomTextGenerator(UnicodeLanguage.ENGLISH, UnicodeLanguage.KOREAN, )
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
